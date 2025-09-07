4ac44150782545f79a0003656b9c7045b58c3cbd
Ebalin1411 <110546356+Ebalin1411@users.noreply.github.com>
2025-09-07 21:05:11 +0530

    Update prisma.ts

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,16 +1,12 @@
-import { withAccelerate } from '@prisma/extension-accelerate';
-import { PrismaClient } from '../generated/prisma';
+// lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ['query', 'error', 'warn'],
+    log: ["error", "warn"], // reduce noise
   });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
-export default prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
